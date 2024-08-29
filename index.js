a96bca43114eb5bb1bee20be813fad06eac2141d
kotesh-arya <koteswarraomudila@gmail.com>
2024-08-29 14:05:28 +0530

    REFACTOR: Backend initialization

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-// const { readFile, readFileSync } = require("fs").promises;
-// // const txt = readFileSync("./hello.txt", "utf8");
-
-// async function sample() {
-//   const file = await readFile("./hello.txt", "utf8");
-//   console.log(file);
-// }
-// sample();
-// // readFile("./hello.txt", "utf8", (err, txt) => {
-// //   console.log(txt);
-// // });
-// console.log("Do this Asap");
-
 const express = require("express");
-const { readFile } = require("fs").promises;
-
+require("dotenv").config();
 const app = express();
-app.get("/", async (request, response) => {
-  //   readFile("./home.html", "utf8", (err, html) => {
-  //     if (err) {
-  //       response.status(500).send("Sorry out of order!!");
-  //     }
-  //     response.send(html);
-  //   });
+const port = process.env.PORT;
 
-  response.send(await readFile("./home.html", "utf8"));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/twitter", (req, res) => {
+  res.send("here we go with the twitter handle");
+});
+app.get("/intro", (req, res) => {
+  res.send("INRTOOO");
+});
+app.get("/login", (req, res) => {
+  res.send(`<h1>Please login</h1>`);
 });
 
-app.listen(3000, () => {
-  console.log(
-    `App is running live to handle requests on http://localhost:3000`
-  );
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+  // console.log(typeof express, "express function here!");
+  // console.log(app, "express app here!");
 });
+console.log(process.env.PORT, "env file");
